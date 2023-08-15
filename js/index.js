8e91d1a69b9adeebad25862c4b5ef9051a208bf0
Muslim0915 <muslimpro0915@gmail.com>
2023-08-15 16:23:01 +0500

    adding folders

---
+++ js/index.js
@@ -0,0 +1,18 @@
+"use strict"
+const nav = document.querySelector('.navbar');
+const nav_menu = document.querySelector('.nav-menu');
+const burger_menu = document.querySelector('.burger-menu');
+const nav_list = document.querySelectorAll('.nav-link');
+for (let i = 0; i < nav_list.length; i++){
+    nav_list[i].onclick = () =>{
+        nav.classList.toggle('active');
+        nav_menu.classList.toggle('active');
+        burger_menu.classList.toggle('active');
+    }
+}
+
+burger_menu.onclick = () =>{
+    nav.classList.toggle('active');
+    nav_menu.classList.toggle('active');
+    burger_menu.classList.toggle('active');
+}
