7c9b55e3df8d7945bc9b679d2567f9978f075473
Muslim0915 <muslimpro0915@gmail.com>
2024-01-04 16:17:30 +0500

    adding changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,4 @@
 "use strict";
-import { Stars } from "./stars.js";
 
 
 const nav = document.querySelector('.navbar');
@@ -9,15 +8,21 @@ const nav_list = document.querySelectorAll('.nav-link');
 
 
 
-
-
-
 for (let i = 0; i < nav_list.length; i++) {
     nav_list[i].onclick = () => {
         nav.classList.toggle('active');
         nav_menu.classList.toggle('active');
         burger_menu.classList.toggle('active');
+        for (let j = 0; j < nav_list.length; j++) {
+            if (j !== i) {
+                nav_list[j].classList.remove('active');
+            }
+            else{
+                nav_list[j].classList.add('active');
+            }
+        }
     }
+
 }
 
 burger_menu.onclick = () => {
